052f5aee90fd3d6ed3334fe75dfc6a7f84865a6d
danielwiener <daniel@danielwiener.com>
2011-08-12 17:26:39 -0400

    trying to figure out placement of footer image. when I move the footer outside of #page, the page loses its bg color not sure why, must be something to do with nesting.

--- themes/familymattersny/footer.php
+++ themes/familymattersny/footer.php
@@ -10,24 +10,16 @@
  */
 ?>
 
-	</div><!-- #main -->
+</div><!-- #main -->
 
-	<footer id="colophon" role="contentinfo">
+<footer id="colophon" role="contentinfo">
 
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with three columns of widgets.
-				 */
-				get_sidebar( 'footer' );
-			?>
-
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
-			</div>
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
+		<div id="site-generator">
+			<?php do_action( 'twentyeleven_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+		</div>
+</footer><!-- #colophon -->
+     </div><!-- #page -->
 <?php wp_footer(); ?>
 
 </body>
