02a9750e6a833734bd0ea9c35e1b8683c839be78
danielwiener <daniel@danielwiener.com>
2011-08-22 15:24:47 -0400

    adding code if there are more than 5 posts (6 counting the intro) use thumbnail and excerpt

--- themes/familymattersny/category-parenting_through_picture_books.php
+++ themes/familymattersny/category-parenting_through_picture_books.php
@@ -27,9 +27,10 @@ get_header(); ?>
 				);
 			   
 				$dw_sticky_query = New WP_Query( $args );
-			   	while ( $dw_sticky_query->have_posts() ) : $dw_sticky_query->the_post(); ?>
-                <?php the_title(); ?><br>
-                <?php the_content(); ?><br>
+			   	while ( $dw_sticky_query->have_posts() ) : $dw_sticky_query->the_post(); ?> 
+			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>  
+
+                <?php the_content(); ?>
 				<?php endwhile; ?> 
 				   <hr />
 			      <?php 
@@ -41,9 +42,17 @@ get_header(); ?>
 							 'post_status'			=> 'published',
 							 'post__not_in' 		=> $dw_sticky
 							);
-						query_posts($args_2); 
+						query_posts($args_2);
+						$picture_book_categories = get_categories('include=3');  //picture book category
+						$total_in_picture_book = '';
+						foreach($picture_book_categories as $picture_book_category) {
+						 $total_in_picture_book += $picture_book_category->count;
+						}
+						 
 									while ( have_posts() ) : the_post(); ?>
-
+                                    <?php if ($total_in_picture_book == 3): ?>
+                                    	<?php the_post_thumbnail( 'thumbnail', array('class' => 'alignleft') ); ?><h2><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1><?php the_excerpt(); ?>        
+                                     <?php else: ?>
 									<?php
 										/* Include the Post-Format-specific template for the content.
 										 * If you want to overload this in a child theme then include a file
@@ -51,9 +60,9 @@ get_header(); ?>
 										 */
 										get_template_part( 'content', get_post_format() );
 									?>
-
+                                  <?php endif; ?>
 								<?php endwhile; ?>
-
+                                  
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
 
 	 
