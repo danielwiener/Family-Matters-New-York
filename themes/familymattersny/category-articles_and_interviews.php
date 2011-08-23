3f6f350ee12588a641f9dba00abfe5c6e0b9b2de
danielwiener <daniel@danielwiener.com>
2011-08-23 13:16:23 -0400

    clean up

--- themes/familymattersny/category-articles_and_interviews.php
+++ themes/familymattersny/category-articles_and_interviews.php
@@ -1,14 +1,13 @@
 <?php
 /**
- * The template for displaying Category Archive pages.
+ * The template for displaying Articles and Interviews pages.
  *
  * @package WordPress
- * @subpackage Twenty_Eleven
- * @since Twenty Eleven 1.0
+ * @subpackage FamilyMattersNY 
+ * @since FamilyMattersNY 1.0
  */
 
 get_header(); ?>
-
 		<section id="primary">
 			<div id="content" role="main">
 				<header class="page-header">
@@ -16,20 +15,13 @@ get_header(); ?>
 						printf( __( '%s', 'twentyeleven' ),  single_cat_title( '', false )  );
 					?></h1>
 				</header>
-
-			  
 						            <?php
 									while ( have_posts() ) : the_post(); ?>
                                     	<?php the_post_thumbnail( 'thumbnail', array('class' => 'alignleft') ); ?> <span class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span> <?php the_excerpt(); ?> 
 <hr />       
 								<?php endwhile; ?>
-                                  
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-	 
-
 			</div><!-- #content -->
 		</section><!-- #primary -->
-
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
