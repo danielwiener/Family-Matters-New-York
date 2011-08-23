26808bcf13ad15b66ee3fcc64030ffc55f7674d9
danielwiener <daniel@danielwiener.com>
2011-08-23 13:11:18 -0400

    adding code for getting thumbnail and excerpt

--- themes/familymattersny/category-articles_and_interviews.php
+++ themes/familymattersny/category-articles_and_interviews.php
@@ -17,41 +17,11 @@ get_header(); ?>
 					?></h1>
 				</header>
 
-				<?php
-				$dw_sticky = get_option( 'sticky_posts' ); 
-				$args = array(
-					'category_name' 		=> 'parenting_through_picture_books',
-					'post_type'				=> 'post',
-					'post_status'			=> 'published',
-					'post__in' 				=> $dw_sticky
-				);
-			   
-				$dw_sticky_query = New WP_Query( $args );
-			   	while ( $dw_sticky_query->have_posts() ) : $dw_sticky_query->the_post(); ?> 
-			<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'twentyeleven' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>  
-
-                <?php the_content(); ?>
-				<?php endwhile; ?> 
-				   <hr />
-			      <?php 
-						global $query_string; 
-						$args_2 = array( 
-							'ignore_sticky_posts' 	=> 1,
-							 'category_name' 		=> 'parenting_through_picture_books',
-							 'post_type'			=> 'post',
-							 'post_status'			=> 'published',
-							 'post__not_in' 		=> $dw_sticky
-							);
-						query_posts($args_2);
-						$picture_book_categories = get_categories('include=3');  //picture book category
-						$total_in_picture_book = '';
-						foreach($picture_book_categories as $picture_book_category) {
-						 $total_in_picture_book += $picture_book_category->count;
-						}
-						 
+			  
+						            <?php
 									while ( have_posts() ) : the_post(); ?>
-                                    <?php if ($total_in_picture_book == 3): ?>  <!-- change to === to 3 for testing -->
-                                    	<?php the_post_thumbnail( 'thumbnail', array('class' => 'alignleft') ); ?> <span class="entry-title"><?php the_title(); ?></span> <?php the_excerpt(); ?> 
+                                    <?php if ( 1 == 1 ): ?>  <!-- change to === to 3 for testing -->
+                                    	<?php the_post_thumbnail( 'thumbnail', array('class' => 'alignleft') ); ?> <span class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span> <?php the_excerpt(); ?> 
 <hr />       
                                      <?php else: ?>
 									<?php
