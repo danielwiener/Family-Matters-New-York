204a4e3d2e04bb50cc6524ce779eafde0d1eeff9
danielwiener <daniel@danielwiener.com>
2011-08-23 13:12:33 -0400

    deleting content-post-format and so loop is on page

--- themes/familymattersny/category-articles_and_interviews.php
+++ themes/familymattersny/category-articles_and_interviews.php
@@ -20,18 +20,8 @@ get_header(); ?>
 			  
 						            <?php
 									while ( have_posts() ) : the_post(); ?>
-                                    <?php if ( 1 == 1 ): ?>  <!-- change to === to 3 for testing -->
                                     	<?php the_post_thumbnail( 'thumbnail', array('class' => 'alignleft') ); ?> <span class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></span> <?php the_excerpt(); ?> 
 <hr />       
-                                     <?php else: ?>
-									<?php
-										/* Include the Post-Format-specific template for the content.
-										 * If you want to overload this in a child theme then include a file
-										 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-										 */
-										get_template_part( 'content', get_post_format() );
-									?>
-                                  <?php endif; ?>
 								<?php endwhile; ?>
                                   
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
